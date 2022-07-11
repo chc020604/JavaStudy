25bfd3656b83545c4e42baedf50ec817a22a323f
ITPS <ITPS@DESKTOP-NUOPUIF>
2022-07-11 15:10:47 +0900

    commit

--- JavaStudy_20220623_ChoHyeonchang/src/j19_제네릭/wildcard/WildCardTestMain.java
+++ JavaStudy_20220623_ChoHyeonchang/src/j19_제네릭/wildcard/WildCardTestMain.java
@@ -8,7 +8,7 @@ public class WildCardTestMain {
 	// <?>라는 건 나는 다양한 자료형을 사용하겠다는 뜻(String등을 고정하는게 아니라)
 	public WildCardTest<?> test(int index){ 
 		if(index == 0) {
-			return new WildCardTest<Integer>(100); // <>안을 비워줘도 정상적으로 실행이 되긴 한다.
+			return new WildCardTest<Integer>(100); // <>안을 비워줘도 정상적으로 실행이 된다.
 		}else if(index == 1) {
 			return new WildCardTest<Boolean>(true);
 		}else if(index == 2) {
@@ -31,6 +31,7 @@ public class WildCardTestMain {
 		Student s = (Student) testMain.test(3).getData(); 
 					//Student 객채로 바꾸려면 꼭 다운캐스팅을 해줘야 한다 -> (Student)
 		System.out.println(s.getStudent_code());
+		
 		((Human) testMain.test2().getData()).readBooks();;
 	}
 
